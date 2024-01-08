aa68efca207d41302d17493f274ed3aca884e2a3
satkumar09 <139046213+satkumar09@users.noreply.github.com>
2024-01-08 07:16:07 +0530

    Add files via upload

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+import Chai from './Chai'
+
+function App() {
+  return ( 
+  //fragment -> <></> -> used to wrap many html tags as you can't return more than one html tags
+  <>           
+  <h1>Hello this is</h1>
+  <Chai/>
+  <h2>Yes.....</h2>
+  </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+  return(
+    <h1>Chai aur React</h1> 
+  )
+}
+
+export default Chai
